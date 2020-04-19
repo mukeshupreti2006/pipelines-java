1ca8117a7b0782b1c8b97f7f9e24af9218833d49
mukeshupreti2006 <mukeshupreti2006@gmail.com>
2020-04-19 19:06:38 +0530

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
             System.out.println("I am covered");
             return;
         }
-
+        System.out.println("I am covered1");
         System.out.println("I am not covered");
     }
 }
